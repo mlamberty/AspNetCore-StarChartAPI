f1b1db4ec1e44e29a94a2be6b89496f779a63a55
mlamberty <mattlamberty@outlook.com>
2021-03-23 14:23:14 -0500

    controller

--- StarChart/Data/ApplicationDbContext.cs
+++ StarChart/Data/ApplicationDbContext.cs
@@ -1,14 +1,17 @@
 ﻿using Microsoft.EntityFrameworkCore;
-
 using StarChart.Models;
+
+
 
 namespace StarChart.Data
 {
     public class ApplicationDbContext : DbContext
     {
+        public DbSet<CelestialObject> CelestialObjects { get; set; }
+
         public ApplicationDbContext(DbContextOptions options) : base(options)
         {
         }
-        DbSet<CelestialObject> CelestialObjects;
+        DbSet<CelestialObject>
     }
 }
--- StarChart/Models/CelestialObject.cs
+++ StarChart/Models/CelestialObject.cs
@@ -5,14 +5,15 @@ using System.Linq;
 using System.Threading.Tasks;
 
 namespace StarChart.Models
-    {
+{
     public class CelestialObject
-        {
+    {
         public int Id { get; set; }
         [Required]
         public string Name { get; set; }
         public int? OrbitedObjectId { get; set; }
+        [NotMapped]
         public List<CelestialObject> Satellites { get; set; }
         public TimeSpan OrbitalPeriod { get; set; }
-        }
     }
+}
